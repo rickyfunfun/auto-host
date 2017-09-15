8a3e0b2e48edbaf3541dbaa1a4b7b13e126ca8fa
Panda <151266405@qq.com>
2017-09-15 18:43:40 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 
 http_code=`curl -o "/etc/hosts_tmp" -w %{http_code}"\n" "https://raw.githubusercontent.com/wangchunming/2017hosts/master/hosts-pc"`
 if [ $http_code -eq 200 ]; then
-  rm "/etc/hosts"
-  mv "/etc/hosts_tmp" "/etc/hosts"
+  sudo rm "/etc/hosts"
+  sudo mv "/etc/hosts_tmp" "/etc/hosts"
   echo "Hosts update completed!"
 else
   echo "Request error!"
